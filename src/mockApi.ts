160ebdcbd22b1caf3e5290d6899d42efbb204743
Phil Heslop <109079089+philipheslop@users.noreply.github.com>
2025-01-23 09:14:53 +0000

    added basic states

--- src/mockApi.ts
+++ src/mockApi.ts
@@ -1,6 +1,8 @@
 import mockData from "./mockData.json";
+import mockStateData from "./mockStateData.json";
 
 let moodletData = mockData;
+let moodletStateData = mockStateData;
 
 export type Moodlet = {
   id: number;
@@ -8,8 +10,24 @@ export type Moodlet = {
   state: string;
 };
 
+export type MoodletState = {
+  name: string;
+  transitions: Transition[];
+};
+
+export type Transition = {
+  action: string;
+  result: string;
+};
+
 export const getMoodlets = () =>
   new Promise<Moodlet[]>((resolve, reject) => {
     if (!moodletData) reject(new Error("Moodlets not found"));
     resolve(Object.values(moodletData));
+  });
+
+export const getMoodletStates = () =>
+  new Promise<MoodletState[]>((resolve, reject) => {
+    if (!moodletStateData) reject(new Error("Moodlet States not found"));
+    resolve(Object.values(moodletStateData));
   });

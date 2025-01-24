88a30f6871bf6cacd684c5d4ec3e5636bfe4fa63
Phil Heslop <109079089+philipheslop@users.noreply.github.com>
2025-01-24 10:38:29 +0000

    state transitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
 import React from "react";
 import "./App.css";
-import { getMoodlets, Moodlet } from "./mockApi";
+import {
+  getMoodlets,
+  getMoodletStates,
+  Moodlet,
+  MoodletState,
+} from "./mockApi";
 import { MoodletComponent } from "./MoodletComponent";
 
 export default function App() {
+  //States for data from mockApi - allowing a data driven approach to defining moodlets, states etc.
   const [moodlets, setMoodlets] = React.useState<Moodlet[] | null>(null);
+  const [moodletStates, setMoodletStates] = React.useState<
+    MoodletState[] | null
+  >(null);
+
+  //Using a single useEffect to fetch data - this would be a proper fetch but just using mock for now
+  //Also, this may work better with different useEffect for each fetch if dependancies are different
   React.useEffect(() => {
     const doGetMoodlets = async () => {
       const result = await getMoodlets();
       setMoodlets(result);
     };
+    const doGetMoodletStates = async () => {
+      const result = await getMoodletStates();
+      setMoodletStates(result);
+    };
     doGetMoodlets();
+    doGetMoodletStates();
   }, []);
 
-  if (!moodlets)
+  if (!moodlets || !moodletStates)
     return (
       <div className="App">
         <h1>No Moodlets Loaded</h1>
@@ -26,7 +43,9 @@ export default function App() {
         {moodlets.map((moodlet) => {
           return (
             <MoodletComponent
+              key={moodlet.id}
               moodlet={moodlet}
+              moodletStates={moodletStates}
               isFullWord={true}
             ></MoodletComponent>
           );
--- src/MoodletComponent.tsx
+++ src/MoodletComponent.tsx
@@ -1,20 +1,61 @@
-import { Moodlet } from "./mockApi";
+import React, { MouseEvent } from "react";
+import { Moodlet, MoodletState, Transition } from "./mockApi";
 import "./MoodletComponent.css";
 
 export interface MoodletComponentProps {
   moodlet: Moodlet;
+  moodletStates: MoodletState[];
   isFullWord: boolean;
 }
 
 export const MoodletComponent: React.FC<MoodletComponentProps> = ({
   moodlet,
+  moodletStates,
   isFullWord,
 }) => {
+  const handleClick = React.useCallback((event: MouseEvent) => {
+    event.preventDefault();
+    console.log("handleclick");
+    console.log(moodlet);
+    let currentState: MoodletState | undefined = moodletStates.find(
+      (state: MoodletState) => state.name === moodlet.state
+    );
+    if (currentState) {
+      let transition = null;
+      switch (event.type) {
+        case "click":
+          transition = currentState.transitions.find(
+            (transition: Transition) => transition.action === "leftclick"
+          );
+          transition ? (moodlet.state = transition.result) : null;
+          break;
+        case "contextmenu":
+          transition = currentState.transitions.find(
+            (transition: Transition) => transition.action === "rightclick"
+          );
+          transition ? (moodlet.state = transition.result) : null;
+          break;
+      }
+      console.log(moodlet);
+    }
+  }, []);
+
+  console.log("handleclick");
+
   return (
     <div
+      style={
+        {
+          "--moodlet-colour": moodlet.state === "required" ? "black" : "grey",
+          "--moodlet-border-colour":
+            moodlet.state === "required" ? "red" : "blue",
+        } as React.CSSProperties
+      }
       className={`moodlet ${
         isFullWord ? "moodlet-fullword" : "moodlet-not-fullword"
       }`}
+      onClick={handleClick}
+      onContextMenu={handleClick}
     >
       {isFullWord ? moodlet.name : moodlet.name[0]}
     </div>
